4cd7c465569152d4e33dc339a3f299be8484ed2e
Prakashpooja <poojaprakash7026@gmail.com>
2025-12-19 11:17:08 +0530

    Updated!

--- versioned_api/api/views_v2.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-
-def user_info_v2(request):
-    return JsonResponse({
-        "version": "v2",
-        "name": "Pooja",
-        "role": "Developer",
-        "location": "India"
-    })
